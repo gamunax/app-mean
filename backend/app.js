b25320d6878c6c01e2c4147cbc0f4c04edc402f1
Jan Pierre <gamunaxx@gmail.com>
2020-10-06 01:46:49 -0500

    feat: integrate services and enable cors

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,37 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('First middleware');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-Width, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.send('Hello from Express =)');
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "xsdsdsd",
+      title: "First server-side port",
+      content: "this is content",
+    },
+    {
+      id: "aaaaaa",
+      title: "Second server-side port",
+      content: "this is content 2",
+    },
+  ];
+  res.status(200).json({
+    message: "succesfully",
+    posts,
+  });
 });
 
 module.exports = app;
